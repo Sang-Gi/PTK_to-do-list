fa23b433ec15c7fd70d0813508595de6f17c6c30
Jack <jackid1103@naver.com>
2023-08-29 15:22:47 +0900

    setting: 초기 개발 환경 셋팅

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <Root>
+      <Header />
+    </Root>
+  );
+}
+
+const Root = styled.div`
+  width: 25rem;
+  height: 100vh;
+  background-color: white;
+  margin: 0 auto;
+`;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default function Header() {
+  return (
+    <Root>
+      <h1>할일 목록</h1>
+    </Root>
+  );
+}
+
+const Root = styled.div`
+  width: 100%;
+`;
