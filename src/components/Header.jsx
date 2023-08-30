da6ec183deed5bc8422d4ac9bdad18c6f5971401
Jack <jackid1103@naver.com>
2023-08-30 16:57:15 +0900

    feat: Header 뷰 마무리

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { styled as mstyled } from "@mui/material/styles";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 export default function Header() {
   const [date, setDate] = useState(() => new Date());
@@ -17,6 +20,7 @@ export default function Header() {
   };
 
   const optionsDate = { year: "numeric", month: "long", day: "numeric" };
+  // padStart? : ES8에 추가되었고, 첫번째 파라미터 길이 만큼 될때까지 두번쨰 파라미터를 앞에 붙여줌 | padEnd는 뒤에
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
@@ -24,10 +28,27 @@ export default function Header() {
   return (
     <Root>
       <Subject>할일 목록</Subject>
-      <span>
+      <p>
         {date.toLocaleDateString(undefined, optionsDate)} {hours}시 {minutes}분{" "}
         {seconds}초
-      </span>
+      </p>
+      <InputBox>
+        <CustomTextField label="Todo..." variant="outlined" size="small" />
+        {/* disableElevation? : MUI의 z축, 즉 기본 쉐도우처리를 지워줌 */}
+        <Button variant="contained" disableElevation>
+          등록
+        </Button>
+      </InputBox>
+      <InputBox>
+        <Button
+          color="success"
+          variant="contained"
+          sx={{ width: "100%" }}
+          disableElevation
+        >
+          그룹 추가
+        </Button>
+      </InputBox>
     </Root>
   );
 }
@@ -38,4 +59,15 @@ const Root = styled.div`
 
 const Subject = styled.h1`
   margin-top: 0;
+`;
+
+const InputBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  margin-bottom: 0.5rem;
+`;
+
+const CustomTextField = mstyled(TextField)`
+  width: 80%;
 `;
