df5ea059a56937534345f5602f043286d7ed33a9
Jack <jackid1103@naver.com>
2023-09-01 10:55:38 +0900

    feat: fetch api 분리

---
+++ src/api/index.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const url = "/data/dummy.json";
+
+export const fetchGetGroupList = async () => {
+  return await axios(url);
+};
+
+export const fetchPostNewGroup = async (groupName) => {
+  const res = await fetchGetGroupList();
+  const lastIdx = res.data.groups[res.data.groups.length - 1].groupId + 1;
+  const newGroupObj = {
+    groupName: groupName,
+    groupId: lastIdx,
+    isSelected: true,
+    toDoList: [],
+  };
+
+  return await axios.post(url, newGroupObj);
+};
