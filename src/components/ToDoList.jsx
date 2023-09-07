18222961d909bfcb93f887eba5ecf588a3823bc9
Jack <jackid1103@naver.com>
2023-09-07 10:47:00 +0900

    feat: 리스트/휴지통 탭 이동

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 import { useRecoilState } from "recoil";
 import GroupList from "./GroupList";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchGetGroupList } from "../api/index";
 import { groupListState } from "../recoil/index";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Button from "@mui/material/Button";
+import { styled as mstyled } from "@mui/material/styles";
 
 export default function ToDoList() {
   const [groups, setGroups] = useRecoilState(groupListState);
+  const [tabStatus, setTabStatus] = useState("list");
 
   useEffect(() => {
     // fetch => group list 불러오기
@@ -22,14 +26,45 @@ export default function ToDoList() {
     fetchData();
   }, [setGroups]);
 
+  const handleHeaderAble = () => {
+    // TODO : 헤더 영역 비활성화 추가
+    setTabStatus("list");
+  };
+
+  const handleHeaderDisable = () => {
+    // TODO : 헤더 영역 비활성화 추가
+    setTabStatus("trash");
+  };
+
   return (
     <Root>
-      <h2>리스트 휴지통</h2>
-      <ListBox>
-        {groups.map((group) => {
-          return <GroupList key={group.groupId} group={group} />;
-        })}
-      </ListBox>
+      <ButtonGroup
+        disableElevation
+        variant="outlined"
+        sx={{ marginBottom: "0.7rem", width: "100%" }}
+      >
+        <CustomButton
+          variant={tabStatus === "list" ? "contained" : "outlined"}
+          onClick={handleHeaderAble}
+        >
+          리스트
+        </CustomButton>
+        <CustomButton
+          variant={tabStatus === "trash" ? "contained" : "outlined"}
+          onClick={handleHeaderDisable}
+        >
+          휴지통
+        </CustomButton>
+      </ButtonGroup>
+      {tabStatus === "list" && (
+        <ListBox>
+          {groups.map((group) => {
+            return <GroupList key={group.groupId} group={group} />;
+          })}
+        </ListBox>
+      )}
+      {/* TODO: 쓰레기통 리스트 추가 */}
+      {tabStatus === "trash" && <ListBox>쓰레기</ListBox>}
     </Root>
   );
 }
@@ -43,4 +78,10 @@ const ListBox = styled.div`
   width: 100%;
   height: 90%;
   overflow-y: auto;
+`;
+
+const CustomButton = mstyled(Button)`
+  width: 100%;
+  font-size: 1rem;
+  font-weight: bold;
 `;
